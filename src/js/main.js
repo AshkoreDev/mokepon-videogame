8ede8e42808b2d0f31692cb976dd69da84ca4f71
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-12-03 00:19:02 +0100

    Added choose pets logic

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,35 @@ import './nodes.js';
 
 // Variables
 let playerPet;
+let opponentPet;
 
 // Functions
+function random(min, max) {
+
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function chooseOpponentPet() {
+
+	opponentPet = random(1,3);
+
+	if (opponentPet == 1) opponentPet = 'Capipepo';
+	else if(opponentPet == 2) opponentPet = 'Hipodoge';
+	else opponentPet = 'Ratigueya';
+
+	opponentPetName.textContent = opponentPet;
+}
+
 function choosePlayerPet() {
 
 	if (capipepoPet.checked) playerPet = 'Capipepo';
 	else if(hipodogePet.checked) playerPet = 'Hipodoge';
 	else if(ratigueyaPet.checked) playerPet = 'Ratigueya';
+	else console.log('Debes seleccionar una mascota.');
 	
-	console.log(playerPet);
+	playerPetName.textContent = playerPet;
+
+	chooseOpponentPet();
 }
 
 
--- src/js/nodes.js
+++ src/js/nodes.js
@@ -11,6 +11,10 @@ const waterAttack = document.getElementById('waterAttack');
 const earthAttack = document.getElementById('earthAttack');
 
 // Messages
+const playerPetName = document.getElementById('playerPetName');
+const playerPetLives = document.getElementById('playerPetLives');
+const opponentPetName = document.getElementById('opponentPetName');
+const opponentPetLives = document.getElementById('opponentPetLives');
 
 // Restart
 const restartBtn = document.getElementById('restartBtn');
